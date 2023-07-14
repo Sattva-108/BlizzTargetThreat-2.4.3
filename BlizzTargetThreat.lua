c4c0acf1a77e9ff40f1833cf458f44bd503a220d
Sattva <Sattvagit@gmail.com>
2023-07-14 06:42:46 +0300

    Make it OnUpdate, print names

--- BlizzTargetThreat.lua
+++ BlizzTargetThreat.lua
@@ -3,10 +3,17 @@ if not ThreatLib then ThreatLib = Threat end
 
 
 local frame = CreateFrame("Frame")
-frame:RegisterEvent("PLAYER_TARGET_CHANGED")
-frame:SetScript("OnEvent", function()
+--frame:RegisterEvent("PLAYER_TARGET_CHANGED")
+frame:SetScript("OnUpdate", function()
+    local player_name = UnitName("player")
+    local target_name = UnitName("target")
     local player_guid = UnitGUID("player")
     local target_guid = UnitGUID("target")
+    if target_guid == nil then return end
     local threat = Threat:GetThreat(player_guid, target_guid)
-    print("Threat for player:", player_guid, "on target:", target_guid, "is", threat)
+    print("Threat for player:", player_name, "on target:", target_name, "is", threat)
 end)
+
+
+
+--local threat = Threat:GetThreat(player_guid, target_guid)
