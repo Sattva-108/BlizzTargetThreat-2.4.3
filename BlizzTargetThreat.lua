a6fa8851adf40aadd18fbf1dd6e87cf6d22ce807
yutsuku <moh@yutsuku.net>
2023-07-14 07:59:55 +0300

    #1 add GUI from:
    https://github.com/yutsuku/KLHThreatMeterBlizz
    thank you - @yutsuku
    
    Signed-off-by: Sattva <Sattvagit@gmail.com>

--- BlizzTargetThreat.lua
+++ BlizzTargetThreat.lua
@@ -1,3 +1,11 @@
+local addon = {}
+
+addon.gui = {
+    ["Frame"] = TargetFrameNumericalThreat,
+    ["bg"] = TargetFrameNumericalThreatBG,
+    ["text"] = TargetFrameNumericalThreatValue,
+}
+
 local Threat = LibStub("Threat-2.0")
 
 local frame = CreateFrame("Frame")
@@ -12,9 +20,34 @@ frame:SetScript("OnUpdate", function()
     local maxThreat, _ = Threat:GetMaxThreatOnTarget(target)
     local myThreat = Threat:GetThreat(player, target)
     local threatPercent = myThreat / maxThreat * 100
-    ChatFrame1:AddMessage(format("%.1f%%", threatPercent))
+
+    -- Update the GUI elements
+    local text = threatPercent
+    if threatPercent == math.floor(threatPercent) then
+        text = string.format("%d", threatPercent)
+    else
+        text = string.format("%.1f", threatPercent)
+    end
+
+
+    addon.gui.bg:SetVertexColor(addon.GetThreatStatusColor(threatPercent))
+    addon.gui.bg:Show()  -- Show the background texture
+
+
+    addon.gui.Frame:Show()
 end)
 
-
-
---local threat = Threat:GetThreat(player_guid, target_guid)
+addon.GetThreatStatusColor = function(percentage)
+    if not percentage then
+        return 0.69, 0.69, 0.69
+    end
+    if percentage >= 90 then
+        return 1.0, 0.0, 0.0
+    elseif percentage >= 75 then
+        return 1.0, 0.6, 0.0
+    elseif percentage >= 55 then
+        return 1.0, 1.0, 0.47
+    else
+        return 0.69, 0.69, 0.69
+    end
+end
