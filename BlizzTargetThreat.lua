7108bc589d2bdba8fd6c769bfae59d7facd2933c
Sattva <Sattvagit@gmail.com>
2023-07-14 07:22:20 +0300

    #1 print percent with conditions

--- BlizzTargetThreat.lua
+++ BlizzTargetThreat.lua
@@ -1,17 +1,18 @@
 local Threat = LibStub("Threat-2.0")
-if not ThreatLib then ThreatLib = Threat end
-
 
 local frame = CreateFrame("Frame")
---frame:RegisterEvent("PLAYER_TARGET_CHANGED")
 frame:SetScript("OnUpdate", function()
-    local player_name = UnitName("player")
-    local target_name = UnitName("target")
-    local player_guid = UnitGUID("player")
-    local target_guid = UnitGUID("target")
-    if target_guid == nil then return end
-    local threat = Threat:GetThreat(player_guid, target_guid)
-    print("Threat for player:", player_name, "on target:", target_name, "is", threat)
+    local player = UnitGUID("player")
+    local target = UnitGUID("target")
+    if target == nil then return end
+    if not target then return end
+    if UnitIsDeadOrGhost("target") then return end
+    if not UnitAffectingCombat("target") then return end
+    if not UnitCanAttack("player", "target") then return end
+    local maxThreat, _ = Threat:GetMaxThreatOnTarget(target)
+    local myThreat = Threat:GetThreat(player, target)
+    local threatPercent = myThreat / maxThreat * 100
+    ChatFrame1:AddMessage(format("%.1f%%", threatPercent))
 end)
 
 
